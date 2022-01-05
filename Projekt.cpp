a3fced5faec0b479430df8ac69b90ccaa3d7a23f
Michalek111 <mic.gwozdz@gmail.com>
2022-01-05 19:38:58 +0100

    0.5

--- Projekt.cpp
+++ Projekt.cpp
@@ -3,33 +3,42 @@
 
 using namespace std;
 
-struct biblioteka
+struct User
 {
-    string iduzytkownika;
-    string idwypozyczonejksiazki;
-  
+ string userID;
+   string borrowBook;
     int liczbawypozyczen;
+    string bookID;
+    string title;
+    
+    void ksiazka()
+    {
+    	cout<<"Podaj id ksiazki"<<endl;
+    	if(BookID)
+	}
+    
+    
 };
     
 int main ()
 {
 
     const int n = 1;
-    biblioteka ksiazki[n];
+    User ksiazki[n];
     for (int i =0 ;i<n;i++)
     {
 cout<< "podaj id uzytkownika: ";
-getline(cin,ksiazki[i].iduzytkownika) ;
+getline(cin,ksiazki[i].userID) ;
 cout<< "podaj id wypozyczonej ksiazki: ";
-getline(cin,ksiazki[i].idwypozyczonejksiazki);
+getline(cin,ksiazki[i].borrowBook);
 cout<<"podaj liczbe wypozyczne: ";
 cin>> ksiazki[i].liczbawypozyczen;
 cin.ignore();
     }
 for (int i=0 ; i<n;i++)
 {
-    cout<<ksiazki[i].iduzytkownika<<"wypozyczyl ";
-    cout<< "\"" << ksiazki[i].idwypozyczonejksiazki<< "\""<<", ";
+    cout<<ksiazki[i].userID<<"wypozyczyl ";
+    cout<< "\"" << ksiazki[i].borrowBook<< "\""<<", ";
     cout<< "liczba wypozyczen: "<<ksiazki[i].liczbawypozyczen<<endl;
 }
 cout<<"ksiazki które nie byly wypo¿yczone"<<endl;
@@ -38,7 +47,7 @@ for( int i =0;i<n;i++)
 	if(ksiazki[i].liczbawypozyczen==0)
 	{
 	
-		cout<<ksiazki[i].idwypozyczonejksiazki<<endl;	
+		cout<<ksiazki[i].borrowBook<<endl;	
 	}
 }
 
